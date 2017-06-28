676927fdcd90c8a3967d8d49ac5ebb39927ffdea
Piotr Sekula <Piotr_Sekula@epam.com>
2017-06-28 12:43:31 +0200

    Extend working skeleton with reading board size

--- src/main/java/pl/javaacademy/tictactoe/Main.java
+++ src/main/java/pl/javaacademy/tictactoe/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Choose starting mark ('X' or 'O')");
         Marks startingMark = null;
+        System.out.println("Choose starting mark ('X' or 'O')");
+        Scanner scanner = new Scanner(System.in);
         while (startingMark == null) {
             try {
-                Scanner scanner = new Scanner(System.in);
                 String userMark = String.valueOf(scanner.nextLine());
                 startingMark = Marks.valueOf(userMark);
             } catch (IllegalArgumentException e) {
@@ -19,5 +19,18 @@ public class Main {
         }
 
         System.out.println("You selected: " + startingMark);
+
+        Integer boardSize = null;
+        System.out.println("Please type size of board");
+        while (boardSize == null) {
+            try {
+                String userSize = String.valueOf(scanner.nextLine());
+                boardSize = Integer.valueOf(userSize);
+            } catch (NumberFormatException e) {
+                System.out.println("You put wrong value, please try again");
+            }
+        }
+
+        System.out.println("Selected board size: " + boardSize);
     }
 }

502bde8890f624a282557bea64806b13628e716a
piotr921 <piotr92345@o2.pl>
2017-06-30 01:16:31 +0200

    Write methods to select fields to check for searching winner from boards.

--- src/main/java/pl/javaacademy/tictactoe/BoardSize.java
+++ src/main/java/pl/javaacademy/tictactoe/BoardSize.java
@@ -1,5 +1,10 @@
 package pl.javaacademy.tictactoe;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 class BoardSize {
 
     private Integer width;
@@ -15,7 +20,29 @@ class BoardSize {
         return selectedField <= width * height;
     }
 
-    int getNumberOfFields() {return width * height;}
+    int getNumberOfFields() {
+        return width * height;
+    }
+
+    List<Integer> getHorizontalIndexes(int startingPoint) {
+        int firstIndex = (startingPoint - 1) * width + 1;
+        int lastIndex = startingPoint * width + 1;
+
+        Stream<Integer> stream = IntStream.range(firstIndex, lastIndex).boxed();
+        List<Integer> result = new ArrayList<>();
+
+        stream.forEach(integer -> result.add(integer));
+        return result;
+    }
+
+    List<Integer> getVerticalIndexes(int startingPoint) {
+        List<Integer> result = new ArrayList<>();
+        while (startingPoint <= width * height) {
+            result.add(startingPoint);
+            startingPoint += width;
+        }
+        return result;
+    }
 
     @Override
     public String toString() {
--- src/test/java/pl/javaacademy/tictactoe/BoardSizeTest.java
+++ src/test/java/pl/javaacademy/tictactoe/BoardSizeTest.java
@@ -3,6 +3,10 @@ package pl.javaacademy.tictactoe;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -26,6 +30,15 @@ public class BoardSizeTest {
         };
     }
 
+    @DataProvider(name = "calcNumberOfFieldsDataProvider")
+    public static Object[][] noOfFieldsDataProvider(){
+        return new Object[][]{
+                {3, 3},
+                {1, 1},
+                {10, 10}
+        };
+    }
+
     @Test(dataProvider = "boardCorrectDataProvider")
     public void testFieldIsInBoardCheckerWithCorrectData(int width, int height, int field){
         String message = String.format("Field %d is not in board: %dx%d size\n", field, width, height);
@@ -36,5 +49,106 @@ public class BoardSizeTest {
     public void testFieldIsInBoardCheckerWithWrongData(int width, int height, int field){
         String message = String.format("Field %d is in board: %dx%d size\n", field, width, height);
         assertFalse(new BoardSize(width, height).isFieldOnBoard(field), message);
+    }
+
+    @Test(dataProvider = "calcNumberOfFieldsDataProvider")
+    public void shouldReturnCorrectNumberOfFields(int width, int height){
+        String message = String.format("Error during calculating no. of fields on %dx%d size\n", width, height);
+        assertEquals(new BoardSize(width, height).getNumberOfFields(), width * height, message);
+
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromFirstRow(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getHorizontalIndexes(1);
+
+        assertEquals(Integer.valueOf(1), indexesList.get(0));
+        assertEquals(Integer.valueOf(2), indexesList.get(1));
+        assertEquals(Integer.valueOf(3), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromSecondRow(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getHorizontalIndexes(2);
+
+        assertEquals(Integer.valueOf(4), indexesList.get(0));
+        assertEquals(Integer.valueOf(5), indexesList.get(1));
+        assertEquals(Integer.valueOf(6), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromThirdRow(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getHorizontalIndexes(3);
+
+        assertEquals(Integer.valueOf(7), indexesList.get(0));
+        assertEquals(Integer.valueOf(8), indexesList.get(1));
+        assertEquals(Integer.valueOf(9), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromNotSquareBoard(){
+
+        BoardSize boardSize = new BoardSize(4, 8);
+        List<Integer> indexesList = boardSize.getHorizontalIndexes(6);
+
+        assertEquals(Integer.valueOf(21), indexesList.get(0));
+        assertEquals(Integer.valueOf(22), indexesList.get(1));
+        assertEquals(Integer.valueOf(23), indexesList.get(2));
+        assertEquals(Integer.valueOf(24), indexesList.get(3));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromFirstColumn(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getVerticalIndexes(1);
+
+        assertEquals(Integer.valueOf(1), indexesList.get(0));
+        assertEquals(Integer.valueOf(4), indexesList.get(1));
+        assertEquals(Integer.valueOf(7), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromSecondColumn(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getVerticalIndexes(2);
+
+        assertEquals(Integer.valueOf(2), indexesList.get(0));
+        assertEquals(Integer.valueOf(5), indexesList.get(1));
+        assertEquals(Integer.valueOf(8), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnAllFieldsNumberToCheckFromThirdColumn(){
+
+        BoardSize boardSize = new BoardSize(3, 3);
+        List<Integer> indexesList = boardSize.getVerticalIndexes(3);
+
+        assertEquals(Integer.valueOf(3), indexesList.get(0));
+        assertEquals(Integer.valueOf(6), indexesList.get(1));
+        assertEquals(Integer.valueOf(9), indexesList.get(2));
+    }
+
+    @Test
+    public void shouldReturnListWithVerticalIndexesOfRectangleBoard(){
+
+        BoardSize boardSize = new BoardSize(4, 8);
+        List<Integer> indexesList = boardSize.getVerticalIndexes(3);
+
+        assertEquals(Integer.valueOf(3), indexesList.get(0));
+        assertEquals(Integer.valueOf(7), indexesList.get(1));
+        assertEquals(Integer.valueOf(11), indexesList.get(2));
+        assertEquals(Integer.valueOf(15), indexesList.get(3));
+        assertEquals(Integer.valueOf(19), indexesList.get(4));
+        assertEquals(Integer.valueOf(23), indexesList.get(5));
+        assertEquals(Integer.valueOf(27), indexesList.get(6));
+        assertEquals(Integer.valueOf(31), indexesList.get(7));
     }
 }

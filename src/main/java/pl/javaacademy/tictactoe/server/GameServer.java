175ab0d36ceb4739bb6daa9827a2325f6335c371
Anna Gawda <a.m.gawda@gmail.com>
2017-07-11 09:30:42 +0200

    Minor fixes

--- src/main/java/pl/javaacademy/tictactoe/server/GameServer.java
+++ src/main/java/pl/javaacademy/tictactoe/server/GameServer.java
@@ -1,22 +1,8 @@
 package pl.javaacademy.tictactoe.server;
 
-import java.io.IOException;
-
 public class GameServer {
     public static void main(String[] args) {
         Server server = new Server();
         server.run();
-        try {
-            System.out.println(server.getMessage());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Integer getFieldNumber() {
-
-        return 0;
     }
 }
